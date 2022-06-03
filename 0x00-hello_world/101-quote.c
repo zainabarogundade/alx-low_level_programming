ab7ff3ea590c6e13dacc45e18c96b2be7d3a5664
zainabarogundade <zainabarogundade646@gmail.com>
2022-06-03 10:45:24 -0700

    last commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,8 @@
-innclude <unistd.h>
+#include <unistd.h>
 /**
  * main - Entry point
- *Return: Always 1(Success)
+ *
+ * Return: Always 1(Success)
  */
 int main(void)
 {
@@ -9,3 +10,4 @@ char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(2, str1, 59);
 return (1);
 }
+
