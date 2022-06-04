73951ef80d5967b6c39d9590b747e37208870477
zainabarogundade <zainabarogundade646@gmail.com>
2022-06-04 07:12:27 -0700

    forth commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,7 +7,8 @@
  */
 int main(void)
 {
-	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";              int i;
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
 
 	for (i = 0; i < 52; i++)
 	{
