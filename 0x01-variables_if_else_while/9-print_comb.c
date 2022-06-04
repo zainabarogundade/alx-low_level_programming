4694a50039ef9ea9b107187b702d1caa50953111
zainabarogundade <zainabarogundade646@gmail.com>
2022-06-04 09:14:35 -0700

    9-print_comb.c commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,15 +2,16 @@
 
 /**
  * main - Prints numbers between 0 to 9 with commas and spaces.
- *
+ * 
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i;
+
 	for (i = 48; i < 58; i++)
 	{
-		putchar(i);
+		putchar(i)
 		if (i != 57)
 		{
 			putchar(',');
