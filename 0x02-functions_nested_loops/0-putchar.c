ee2eb89908ec60f660b759a228f9450ebc8afe9b
zainabarogundade <zainabarogundade646@gmail.com>
2022-06-07 04:40:09 -0700

    first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+
+#include<main.h>
+
+/**
+ * main - Prints "_putchar" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('\n');
+
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This README.md file is for 0x02-functions_nested_loops
