a92b8efb8462aaca49a64fd81664499ca9ecef48
zainabarogundade <zainabarogundade646@gmail.com>
2022-06-07 06:51:24 -0700

    second commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 {
 	char letter;
 
-	for(letter = 'a'; lette <= 'z'; letter; letter++)
+	for(letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 
 	_putchar('\n');
