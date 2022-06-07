58d446f073fe7d441459b38cb4d936d500d57bd1
zainabarogundade <zainabarogundade646@gmail.com>
2022-06-07 06:43:10 -0700

    second commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void);
+{
+	char letter;
+
+	for(letter = 'a'; lette <= 'z'; letter; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
