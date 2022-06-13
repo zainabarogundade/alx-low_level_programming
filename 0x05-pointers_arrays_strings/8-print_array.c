e9b7eeaf91c084056f937af9b18fe99951d7a01e
zainabarogundade <zainabarogundade646@gmail.com>
2022-06-13 06:41:31 -0700

    another commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_array - prints n elements of an array of integers.
  * @a: input array.
@@ -15,7 +15,7 @@ void print_array(int *a, int n)
 		printf("%d", *(a + i));
 		if (i != (n - 1))
 			printf(", ");
-
 	}
 	printf("\n");
 }
+
