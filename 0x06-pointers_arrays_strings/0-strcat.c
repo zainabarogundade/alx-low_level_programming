b1a61afaf697951617a2e9fd15727603c5726d9e
zainabarogundade <zainabarogundade646@gmail.com>
2022-06-16 03:45:10 -0700

    initial commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,14 +14,15 @@ char *_strcat(char *dest, char *src)
 	{
 		count++;
 	}
-
+	
 	while (count2 >= 0)
 	{
 		*(dest + count) = *(src + count2);
-		if (*(src + count2) == '\0')if (*(src + count2) == '\0')
+		if (*(src + count2) == '\0')
 			break;
 		count++;
 		count2++;
 	}
 	return (dest);
 }
+
