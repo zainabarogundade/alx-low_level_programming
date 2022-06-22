dce1fc9852e3b8841a6b16a5e559913754ce2272
zainabarogundade <zainabarogundade646@gmail.com>
2022-06-22 15:21:09 -0700

    another commit

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -14,7 +14,7 @@ int _strlen_recursion(char *s)
 }
 
 /**
- * comparator - compares each character of the string
+ * comparator - compares each character of the string.
  * @s: string
  * @n1: smallest iterator.
  * @n2: biggest iterator.
@@ -33,8 +33,8 @@ int comparator(char *s, int n1, int n2)
 
 /**
  * is_palindrome - detects if a string is a palindrome.
- * @s: string.
- * Return: 1 if s is a palindrome, 0 if not.
+ * @s: string
+ * Return: 1 if s is a palindrome, 0 if not
  */
 int is_palindrome(char *s)
 {
