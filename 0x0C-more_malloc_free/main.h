74bb5ed71e03d5be6cf0f5410d63cf583dc7acf8
zainabarogundade <zainabarogundade646@gmail.com>
2022-07-18 08:36:05 -0700

    another comit

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,7 +7,7 @@ int _strlen(char *s);
 char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
+char *string_nconcat(char *s1, char *s2, unsined int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 char *_memset(char *s, char b, unsigned int n);
 int *array_range(int min, int max);
@@ -15,3 +15,4 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif
+
