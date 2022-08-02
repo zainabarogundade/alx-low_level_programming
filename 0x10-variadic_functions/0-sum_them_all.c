6ce824280ae8a6f8f035220d6e44a51d79114968
zainabarogundade <zainabarogundade646@gmail.com>
2022-08-02 04:44:58 -0700

    initial commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,7 +3,7 @@
 /**
  * sum_them_all - returns the sum of all its parameters.
  * @n: amount of the arguments.
- * 
+ *
  * Return: sum of its parameters.
  */
 int sum_them_all(const unsigned int n, ...)
